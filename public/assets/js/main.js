67f20125c02b57e5577fe4e187f1139685b45603
nationlearns <contact@nationlearns.com>
2023-10-31 14:48:04 +0530

    latest

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,82 +1,88 @@
 (function($) {
-  
-  "use strict";  
+	"use strict"
 
-  $(window).on('load', function() {
+	// Scrollspy
+	$('body').scrollspy({
+		target: '#nav',
+		offset: $(window).height() / 2
+	});
 
-  /*Page Loader active
-    ========================================================*/
-    $('#preloader').fadeOut();
+	// Mobile nav toggle
+	$('.navbar-toggle').on('click',function() {
+		$('.main-nav').toggleClass('open');
+	});
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 200) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
-    });
+	// Fixed nav
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+		wScroll > 50 ? $('#header').addClass('fixed-navbar') : $('#header').removeClass('fixed-navbar');
+	});
 
-    /* ==========================================================================
-       countdown timer
-       ========================================================================== */
-     jQuery('#clock').countdown('2018/06/21',function(event){
-      var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Days</div> '
-      +'<div class="time-entry hours"><span>%H</span> Hours</div> '
-      +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
-      +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
-    });
+	// Smooth scroll
+	$(".main-nav a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 800);
+	});
 
-    /* slicknav mobile menu active  */
-    $('.mobile-menu').slicknav({
-        prependTo: '.navbar-header',
-        parentTag: 'liner',
-        allowParentLinks: true,
-        duplicate: true,
-        label: '',
-      });
+	// Section title animation
+	$('.section-title').each(function() {
+		var $this = $(this);
+		$this.find('.title > span').each(function(i) {
+			var $span = $(this);
+			var animated = new Waypoint({
+				element: $this,
+				handler: function()
+				{
+					setTimeout(function(){
+						$span.addClass('appear')
+					}, i*250);
+					this.destroy();
+				},
+				offset: '95%'
+			});
+		});
+	});
 
-      /* WOW Scroll Spy
-    ========================================================*/
-     var wow = new WOW({
-      //disabled for mobile
-        mobile: false
-    });
-    wow.init();
+	// Galery Owl
+	$('#galery-owl').owlCarousel({
+		items:1,
+		loop:true,
+		margin:0,
+		dots : false,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		autoplay : true,
+		autoplaySpeed :500,
+		navSpeed :500,
+		responsive : {
+	    0 : {
+	       stagePadding : 0,
+	    },
+	    768 : {
+	        stagePadding : 120,
+	    }
+		}
+	});
 
-    /* Nivo Lightbox 
-    ========================================================*/
-    $('.lightbox').nivoLightbox({
-        effect: 'fadeScale',
-        keyboardNav: true,
-      });
+	// Parallax Background
+	$.stellar({
+		responsive: true
+	});
 
-    // one page navigation 
-    $('.navbar-nav').onePageNav({
-            currentClass: 'active'
-    }); 
+	// CountTo
+	$('.counter').each(function() {
+		var $this = $(this);
+		var counter = new Waypoint({
+			element: $this,
+			handler: function()
+			{
+				$this.countTo();
+			},
+			offset: '95%'
+		});
+	});
 
-    /* Back Top Link active
-    ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
-
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
-
-  });      
-
-}(jQuery));
+})(jQuery);
