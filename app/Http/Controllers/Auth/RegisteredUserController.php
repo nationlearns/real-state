1cfbe5da576d6b661fab39f34e87cc4117fbdce5
nationlearns <contact@nationlearns.com>
2023-11-02 10:15:15 +0530

    register Api for user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,40 +12,87 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use App\Models\UserOtp;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display the registration view.
-     */
+
     public function create(): View
     {
         return view('auth.register');
     }
 
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): RedirectResponse
+    public function createUser(Request $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        // $request->validate([
+        //     'name' => ['required', 'string', 'max:255'],
+        //     'email' => ['required', 'string', 'lowercase', 'email', 'max:255'],
+        // ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'full_name' => $request->full_name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'phone' => $request->phone,
+            'intrested_in' => $request->intrested_in,
+            'budget' => $request->budget,
+            'prefered_city' =>$request->prefered_city,
+            'required_loans'=> $request->required_loans,
+            'prefered_bank'  => $request->prefered_bank,
+            'marital_status' => $request->marital_status,
+            'city_id' => $request->city_id,
+            'state_id' => $request->state_id,
+
         ]);
 
-        event(new Registered($user));
-
-        Auth::login($user);
-
-        return redirect(RouteServiceProvider::HOME);
+        return response()->json(['status'=>400,'message' => 'User Created Successfully', 'userData' => $user]);
     }
+
+
+    public function sendOTP($sender, $numbers, $message, $otp){
+
+        $apiKey = urlencode('MDhlMTdhOWIzYWI2ODIzMzQxYWNiZmYwNTY0Mzc1NjE=');
+
+        // Prepare data for POST request
+        // $data = array('apikey' => $apiKey);
+
+        session()->put('session_otp', $otp);
+        session()->put('mobile_number', $numbers);
+
+        // $request->session()->put('session_otp', $otp);
+        // $request->session()->put('mobile_number', $numbers);
+
+        // Message details
+        $numbers = urlencode($numbers) ;
+        $sender = urlencode($sender);
+       
+        $data = 'apikey=' . $apiKey . '&numbers=' . $numbers . "&sender=" . $sender . "&message=" . $message;
+
+        // Send the POST request with cURL
+        $ch = curl_init('https://api.textlocal.in/send/');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $timestamp =  $_SERVER["REQUEST_TIME"];
+        session()->put('otp_time', $timestamp);
+    }
+
+    public function requestForOtp(Request $request)
+    {
+        $sender = 'NATLER';
+        $numbers = $request->input('mobileNumber');
+        $otp = rand(100000, 999999);
+        $message = rawurlencode('Dear user Please use this OTP : '.$otp.' to continue on Nation Learns.');
+
+        $otpData= UserOtp::create([
+            'otp' => $otp,
+            'mobile'=> $numbers
+        ]);
+
+        $this->sendOTP($sender, $numbers, $message, $otp);
+        return true;
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/create-user', [RegisteredUserController::class, 'createUser']);
+Route::post('/request-otp', [RegisteredUserController::class, 'requestForOtp']);
+
